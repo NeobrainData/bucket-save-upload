61360f277110f413c216312c31eb7dfa89721d54
rennanvoa2 <rennanvoa2@lwmail.com.br>
2022-07-28 09:05:20 -0300

    Change setup and req

--- setup.py
+++ setup.py
@@ -1,3 +1,21 @@
-from setuptools import setup, find_packages
+import setuptools
 
-setup(name='bucket_save', version='1.0', packages=find_packages())
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='bucket_save',
+    version='1.0',
+    author='Rennan Valadares',
+    author_email='rennanvoa2@gmail.com',
+    description='Save files in a GCS bucket',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/NeobrainData/bucket-save',
+    project_urls = {
+        "Bug Tracker": "https://github.com/NeobrainData/bucket-save/issues"
+    },
+    license='None',
+    packages=['bucket_save'],
+    install_requires=['requests'],
+)
